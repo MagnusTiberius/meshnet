3bcc1daeb287a3c4b2208e0913d5c272fd53a21b
MagnusTiberius <bgonzales20001@gmail.com>
2017-09-24 20:10:33 -0700

    add tls and non tls listener

--- server/main.go
+++ server/main.go
@@ -1,16 +1,131 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
 
 	"github.com/MagnusTiberius/meshnet/api/lex"
 )
 
+var (
+	connPool  map[string]*net.Conn
+	port      string
+	ip        string
+	relayConn net.Conn
+	tlsOk     string
+)
+
+//Event event type
+type Event struct {
+	Name   string
+	Client net.Conn
+	Msg    []byte
+}
+
 func main() {
+	if len(os.Args) == 2 {
+		tlsOk = os.Args[1]
+	}
+
+	if tlsOk == "tls" {
+		tlsServer()
+	} else {
+		setupListener("127.0.0.1", "8049")
+	}
+}
+
+func setupListener(ip string, sport string) {
+	nport, _ := strconv.Atoi(sport)
+	n := nport + 1
+
+	addr := fmt.Sprintf("%s:%d", ip, n)
+	server, err := net.Listen("tcp", addr)
+
+	if server == nil {
+		panic(fmt.Sprintf("%s: %v", "Listen failure: ", err))
+	}
+	connPool = map[string]*net.Conn{}
+	conns := handleConns(server)
+	for {
+		go handleConn(<-conns)
+	}
+
+}
+
+func handleConns(l net.Listener) chan net.Conn {
+	ch := make(chan net.Conn)
+	i := 0
+	go func() {
+		for {
+			client, err := l.Accept()
+			if client == nil {
+				panic(fmt.Sprintf("%s: %v", "Listener Accept() failure: ", err))
+				//continue
+			}
+			i++
+			fmt.Printf("%d: %v accepted %v\n", i, client.LocalAddr(), client.RemoteAddr())
+			connPool[fmt.Sprintf("%v", client.RemoteAddr())] = &client
+			client.Write([]byte("Welcome to echoserver utopia\n"))
+			handleEvent(Event{Name: "CONNECT_EVENT", Client: client})
+			ch <- client
+		}
+	}()
+	return ch
+}
+
+func handleEvent(e Event) {
+	//fmt.Println(e.Name)
+	switch e.Name {
+	case "CONNECT_EVENT":
+		for _, c := range connPool {
+			conn := *c
+			addr := fmt.Sprintf("%v", e.Client.RemoteAddr())
+			if fmt.Sprintf("%v", conn.RemoteAddr()) != addr {
+				msgConnect := []byte(fmt.Sprintf("%s has connected\n", addr))
+				conn.Write(msgConnect)
+			}
+		}
+	case "CLIENT_MSG":
+		for _, c := range connPool {
+			addr := fmt.Sprintf("%v", e.Client.RemoteAddr())
+			conn := *c
+			if fmt.Sprintf("%v", conn.RemoteAddr()) != addr {
+				msg := fmt.Sprintf("ECHO>>%v:%s", e.Client.RemoteAddr(), string(e.Msg))
+				conn.Write([]byte(msg))
+			}
+		}
+	case "RELAY_MSG":
+		for _, c := range connPool {
+			client := *c
+			msg := fmt.Sprintf("RELAY>>%v:%s\n", e.Client.RemoteAddr(), string(e.Msg))
+			client.Write([]byte(msg))
+		}
+	default:
+	}
+	//relayEvent(e)
+}
+
+func handleConn(c net.Conn) {
+	b := bufio.NewReader(c)
+	for {
+		msg, err := b.ReadBytes('\n')
+		if err != nil {
+			break
+		}
+		fmt.Printf("%v:%s", c.RemoteAddr(), string(msg))
+		c.Write(msg)
+		handleEvent(Event{Name: "CLIENT_MSG", Client: c, Msg: msg})
+	}
+}
+
+func tlsServer() {
 	cert, err := tls.LoadX509KeyPair("secure/certs/server.pem", "secure/certs/server.key")
 	if err != nil {
 		log.Fatalf("server: loadkeys: %s", err)
@@ -39,7 +154,8 @@ func main() {
 				log.Print(x509.MarshalPKIXPublicKey(v.PublicKey))
 			}
 		}
-		go handleClient(conn)
+		//go handleClient(conn)
+		go handleConn(conn)
 	}
 }
 
@@ -57,7 +173,7 @@ func handleClient(conn net.Conn) {
 		}
 
 		l := lex.PrvLexer{}
-		l.accept("1234567890")
+		l.Lex(buf)
 
 		log.Printf("server: conn: echo %q\n", string(buf[:n]))
 		n, err = conn.Write(buf[:n])
