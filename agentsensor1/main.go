453f40e098c6e67bcf8e6f43d1bf15f69fcc31a3
MagnusTiberius <bgonzales20001@gmail.com>
2017-09-29 23:32:51 -0700

    Cleanup, remove commented code

--- agentsensor1/main.go
+++ agentsensor1/main.go
@@ -38,38 +38,6 @@ func main() {
 
 	command.Connect(packet, tls)
 
-	/*
-		msg := command.NewMessage()
-		msg.Topic = "welcome/all"
-		msg.Payload = []byte("Hey this is a hello message.\n")
-
-		log.Println("Calling Publish")
-		command.Publish(msg, tls)
-
-		msg.Payload = []byte("Another comment going in.\n")
-		log.Println("Calling Publish")
-		command.Publish(msg, tls)
-
-		subp := command.NewSubscribePacket()
-		subp.PacketID = uid
-		uid = uid + 1
-		sub := command.NewSubscription()
-		sub.Topic = "welcome/all"
-		subp.Subscriptions = append(subp.Subscriptions, sub)
-		sub2 := command.NewSubscription()
-		sub2.Topic = "goodbye/all"
-		subp.Subscriptions = append(subp.Subscriptions, sub2)
-		sub3 := command.NewSubscription()
-		sub3.Topic = "awesome/all"
-		subp.Subscriptions = append(subp.Subscriptions, sub3)
-		log.Printf("Calling Subscribe: %v \n", subp)
-		command.Subscribe(subp, tls)
-
-		msg.Payload = []byte("I would like to see all of them.\n")
-		log.Println("Calling Publish")
-		command.Publish(msg, tls)
-	*/
-
 	fh := client.FuncHandler{
 		OnConnect:     OnConnect,
 		OnPublish:     OnPublish,
